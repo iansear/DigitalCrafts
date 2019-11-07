241d961a0a218fbc2deaaa5f0c3bec8144819ad4
Ian Sear <iansear@Ians-MacBook-Pro.local>
2019-11-07 14:56:22 -0600

    added input checks in calculator

--- week1/day2assignments/calculator.py
+++ week1/day2assignments/calculator.py
@@ -4,10 +4,21 @@ def calculator():
   return ans
 
 def get_inputs():
-  first_num = float(input("Enter first number: "))
+  first_num = get_valid_num("Enter first number: ")
   operator = input("Enter operation (+, -, *, /): ")
-  second_num = float(input("Enter second number: "))
+  second_num = get_valid_num("Enter second number: ")
   return first_num, operator.strip(), second_num
+
+def get_valid_num(message):
+    is_num = False
+    num = 0
+    while is_num == False:
+      try:
+        num = float(input(message))
+        is_num = True
+      except ValueError:
+        print("Not a number, try again...")
+    return num
 
 def determine_operation(first_num, operator, second_num):
   if(operator == "+"):
