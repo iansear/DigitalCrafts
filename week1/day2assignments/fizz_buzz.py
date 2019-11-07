d81473b241e6a60f64dc06c846824a511e7f4ecc
Ian Sear <iansear@Ians-MacBook-Pro.local>
2019-11-07 15:41:42 -0600

    improved fizz buzz number input

--- week1/day2assignments/fizz_buzz.py
+++ week1/day2assignments/fizz_buzz.py
@@ -4,8 +4,19 @@ def fizz_buzz():
   return solution
 
 def get_input():
-  number = int(input("Enter an integer: "))
+  number = get_valid_num("Enter an integer: ")
   return number
+
+def get_valid_num(message):
+    is_num = False
+    num = 0
+    while is_num == False:
+      try:
+        num = int(input(message))
+        is_num = True
+      except ValueError:
+        print("Not a number, try again...")
+    return num
 
 def get_solution(number):
   if (number % 5 == 0) and (number % 3 == 0):
